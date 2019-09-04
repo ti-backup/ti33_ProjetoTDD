c8d27e030cfe9806227ac5eac9f09f7b6d6e5ae3
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-09-04 14:10:46 -0300

    Criação dos projetos Matematica e MatematicaTeste na biblioteca Matematica no Visual Studio

---
+++ Matematica/MatematicaTeste/MatTeste.cs
@@ -0,0 +1,15 @@
+using Matematica;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MatematicaTeste
+{
+    [TestClass]
+    public class MatTeste
+    {
+        [TestMethod]
+        public void RaizQuadradaBasicaTeste()
+        {
+            Mat r = new Mat();
+        }
+    }
+}
