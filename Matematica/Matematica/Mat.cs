6a1cbeda9a3fdb1cad6d52d196f02296fc738f74
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-09-04 17:30:33 -0300

    implementação de um teste para o método MediaAritmetica

--- Matematica/Matematica/Mat.cs
+++ Matematica/Matematica/Mat.cs
@@ -4,9 +4,26 @@ namespace Matematica
 {
     public class Mat
     {
+        public static string msgErroRaizValorNegativo = "Não existe solução no conjunto dos Reais.";
+
         public double RaizQuadrada(double entrada)
         {
+            if (entrada < 0)
+                throw new ArgumentOutOfRangeException("entrada", entrada, msgErroRaizValorNegativo);
+
             return Math.Pow(entrada, 1.0 / 2.0);
+        }
+
+        public double MediaAritmetica(double[] numeros)
+        {
+            double soma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                soma += numeros[i];
+            }
+
+            return soma / (double) numeros.Length;
         }
     }
 }
--- Matematica/MatematicaTeste/MatTeste.cs
+++ Matematica/MatematicaTeste/MatTeste.cs
@@ -1,5 +1,6 @@
 using Matematica;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace MatematicaTeste
 {
@@ -26,6 +27,39 @@ namespace MatematicaTeste
             double resultadoReal = m.RaizQuadrada(entrada);
 
             Assert.AreEqual(esperado, resultadoReal, esperado / 100);
+        }
+
+        [TestMethod]
+        public void RaizQuadradaValorNegativoTeste()
+        {
+            Mat m = new Mat();
+            double entrada = -1.0;
+
+            try
+            {
+                m.RaizQuadrada(entrada);
+            }
+            catch (ArgumentOutOfRangeException ae)
+            {
+                StringAssert.Contains(ae.Message, Mat.msgErroRaizValorNegativo);
+                return;
+            }
+
+            Assert.Fail("O método não lançou a exceção esperada");
+        }
+
+        [TestMethod]
+        public void MediaAritmeticaVetorComDoisValores()
+        {
+            Mat m = new Mat();
+            double[] numeros = new double[2];
+            numeros[0] = 5.0;
+            numeros[1] = 7.0;
+
+            double valorEsperado = 6.0;
+            double valorReal = m.MediaAritmetica(numeros);
+
+            Assert.AreEqual(valorEsperado, valorReal, 0.01);
         }
     }
 }
