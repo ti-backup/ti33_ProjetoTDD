12781adf65355cf61a1d18caac12b49478212651
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-09-04 14:33:11 -0300

    início da implementação dos casos de teste e desenvolvimento do método RaizQuadrada

--- Matematica/Matematica/Mat.cs
+++ Matematica/Matematica/Mat.cs
@@ -4,6 +4,9 @@ namespace Matematica
 {
     public class Mat
     {
-
+        public double RaizQuadrada(double entrada)
+        {
+            return Math.Pow(entrada, 1.0 / 2.0);
+        }
     }
 }
--- Matematica/MatematicaTeste/MatTeste.cs
+++ Matematica/MatematicaTeste/MatTeste.cs
@@ -9,7 +9,23 @@ namespace MatematicaTeste
         [TestMethod]
         public void RaizQuadradaBasicaTeste()
         {
-            Mat r = new Mat();
+            Mat m = new Mat();
+            double esperado = 2.0;
+            double entrada = esperado * esperado;
+            double resultadoReal = m.RaizQuadrada(entrada);
+
+            Assert.AreEqual(esperado, resultadoReal, esperado / 100);
+        }
+
+        [TestMethod]
+        public void RaizQuadradaInteiroPositivoTeste()
+        {
+            Mat m = new Mat();
+            double esperado = 5.0;
+            double entrada = esperado * esperado;
+            double resultadoReal = m.RaizQuadrada(entrada);
+
+            Assert.AreEqual(esperado, resultadoReal, esperado / 100);
         }
     }
 }
